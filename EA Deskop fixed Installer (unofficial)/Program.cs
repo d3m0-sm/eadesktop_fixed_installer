588e2718e43ad0e6cff71b945b2f6146591902af
d3m0-sm <d3m0_sm@posteo.de>
2021-08-24 14:43:46 +0200

    -added fix for start menue entry -added desktop shortcut creation to dialog

--- EA Deskop fixed Installer (unofficial)/Program.cs
+++ EA Deskop fixed Installer (unofficial)/Program.cs
@@ -8,10 +8,13 @@ using System.Diagnostics;
 using System.IO;
 using System.Threading;
 using System.Windows;
+using System.Runtime.InteropServices;
+
 namespace EA_Deskop_fixed_Installer__unofficial_
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             tools eadt = new tools();
@@ -49,6 +52,9 @@ namespace EA_Deskop_fixed_Installer__unofficial_
             string[] xmlLines;
             string tempPath = string.Empty;
             string msiLine = string.Empty;
+            string defaultDir = @"C:\Program Files\Electronic Arts\EA Desktop\EA Desktop";
+            string installDir = string.Empty;
+            string ny = string.Empty;
             
             eadt.eadkill();
             
@@ -94,8 +100,7 @@ namespace EA_Deskop_fixed_Installer__unofficial_
             }
 
             int startInt = msiLine.IndexOf("DownloadUrl");
-            msiLine = msiLine.Substring(startInt);
-            msiLine = msiLine.Replace("DownloadUrl=\"", null);
+            msiLine = msiLine.Substring(startInt).Replace("DownloadUrl=\"", null);
             startInt = msiLine.IndexOf("\"");
             installerURL = msiLine.Remove(startInt);
             startInt = installerURL.LastIndexOf("/");
@@ -111,14 +116,77 @@ namespace EA_Deskop_fixed_Installer__unofficial_
 
             Console.WriteLine();
             Console.WriteLine("Starting installer...");
+            Console.WriteLine();
+            Console.WriteLine("Please go trough the EA Desktop installation setup and come back if its finished!");
             Process.Start(installerName.Replace(".exe", null));
 
             Console.WriteLine();
+            Console.WriteLine("If the setup is finished, please press return...");
+            Console.ReadLine();
+            
+            if (Directory.Exists(defaultDir) == true)
+            {
+                installDir = defaultDir;
+            }
+            else
+            {
+                Console.WriteLine("EA Desktop isnt't installed at the default location.");
+                Console.WriteLine("Do you use a custom instalation location? (y/n)");
+                ny = Console.ReadLine();
+                do
+                {
+                    ny = Console.ReadLine();
+                } while (ny != "y" || ny != "n");
 
-            Console.WriteLine("Go through the Installer in your EA Desktop should be installed sucessfully!");
+                Console.WriteLine();
+
+                if (ny == "y")
+                {
+                    
+                    Console.WriteLine("Please copy paste your EA Desktop installation Path");
+                    installDir = Console.ReadLine();
+                }
+                else if (ny == "n")
+                {
+                    Console.WriteLine("EA Desktop installation not found, please try again!");
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                }
+            }
+
+            File.Delete(@"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\EA\EA Desktop");
+            tools.CreateShortcut("EA Desktop", @"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\EA", installDir + @"\EADesktop.exe");
+            Console.WriteLine("Fixed Start Menu entry");
             Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("You can close this window now");
+
+            ny = string.Empty;
+            if (installDir != string.Empty)
+            {
+                Console.WriteLine("Do you want to create a Desktop shortcut for EA Desktop? (y/n)");
+                do
+                {
+                    ny = Console.ReadLine();
+                } while (ny != "y" && ny != "n");
+
+                if (ny == "y")
+                {
+                    tools.CreateShortcut("EA Desktop", @"C:\Users\" + Environment.UserName + @"\Desktop", installDir + @"\EADesktop.exe");
+                    Console.WriteLine("Shortcut created");
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+
+
 
             Console.ReadKey();
 
--- EA Deskop fixed Installer (unofficial)/tools.cs
+++ EA Deskop fixed Installer (unofficial)/tools.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
 using System.Net.NetworkInformation;
+using IWshRuntimeLibrary;
 
 namespace EA_Deskop_fixed_Installer__unofficial_
 {
@@ -43,6 +44,18 @@ namespace EA_Deskop_fixed_Installer__unofficial_
             }
             return success;
         }
-    
+
+        public static void CreateShortcut(string shortcutName, string shortcutPath, string targetFileLocation)
+        {
+            string shortcutLocation = System.IO.Path.Combine(shortcutPath, shortcutName + ".lnk");
+            WshShell shell = new WshShell();
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutLocation);
+
+            //shortcut.Description = "My shortcut description";   // The description of the shortcut
+            //shortcut.IconLocation = @"c:\myicon.ico";           // The icon of the shortcut
+            shortcut.TargetPath = targetFileLocation;                 // The path of the file that will launch when the shortcut is run
+            shortcut.Save();                                    // Save the shortcut
+        }
+
     }
 }
